1f2bf66c263194fb48fe5759657cc6374a94a45e
lunainwoozu <78474488+lunainwoozu@users.noreply.github.com>
2025-12-22 16:06:17 +0900

    Title: 숫자 피라미드 2, Time: 69ms, Memory: 11MB, Status: Passed - Codetree

--- 251222/숫자 피라미드 2/number-pyramid-2.js
+++ 251222/숫자 피라미드 2/number-pyramid-2.js
@@ -1,11 +1,13 @@
 const fs = require('fs');
 let n = Number(fs.readFileSync(0));
 
-for (let i = 0; i < n; i++){
-    let result = '';
-    for (let j = 1; j <= i + 1; j++){
-        result += i + j;
-        if (j <= i) result += " ";
+let cnt = 1;
+
+for (let i = 1; i <= n; i++){
+    let result = "";
+    for (let j = 1; j <= i; j++){
+        result += cnt + " ";
+        cnt++;
     }
     console.log(result);
 }
