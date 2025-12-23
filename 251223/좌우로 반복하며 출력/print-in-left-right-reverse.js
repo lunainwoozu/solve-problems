8c13502bfd187c20a113a7d4d75d531d7d8a25df
lunainwoozu <78474488+lunainwoozu@users.noreply.github.com>
2025-12-23 15:48:34 +0900

    Title: 좌우로 반복하며 출력, Time: 65ms, Memory: 10MB, Status: Passed - Codetree

--- 251223/좌우로 반복하며 출력/print-in-left-right-reverse.js
+++ 251223/좌우로 반복하며 출력/print-in-left-right-reverse.js
@@ -2,19 +2,14 @@ const fs = require('fs');
 const n = Number(fs.readFileSync(0));
 
 for(let i = 1; i <= n; i++){
-    let row = [];
-    for (let j = 1; j <= n; j++){
-        row.push(j);
-    }
-
     let result = "";
     if (i % 2 === 0){
-        for (let j = n - 1; j >= 0; j--){
-            result += row[j];
+        for (let j = n; j >= 1; j--){
+            result += j;
         }
     } else {
-        for (let j = 0; j < n; j++){
-            result += row[j]
+        for (let j = 1; j <= n; j++){
+            result += j;
         }
     }
     console.log(result);
