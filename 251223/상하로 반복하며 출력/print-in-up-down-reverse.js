e44a3a5aded38c2329f1094d6229653bd9bdc3fc
lunainwoozu <78474488+lunainwoozu@users.noreply.github.com>
2025-12-23 16:23:13 +0900

    Title: 상하로 반복하며 출력, Time: 64ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251223/상하로 반복하며 출력/print-in-up-down-reverse.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const n = Number(fs.readFileSync(0).toString().trim());
+
+for(let i = 0; i < n; i++){
+    str = '';
+    for(let j = 0; j < n; j++){
+        if (j % 2 === 0) str += i + 1;
+        else str += n - i;
+    }
+    console.log(str);
+}
