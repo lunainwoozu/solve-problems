7880d681dcd11db1d646208f76c3dd7b7ccdce11
lunainwoozu <78474488+lunainwoozu@users.noreply.github.com>
2025-12-25 21:18:35 +0900

    Title: 데이터 교환, Time: 57ms, Memory: 10MB, Status: Passed - Codetree

--- 251225/데이터 교환/exchange-data.js
+++ 251225/데이터 교환/exchange-data.js
@@ -1,7 +1,9 @@
-let [a, b, c] = [5,6,7];
-let b = a;
-let c = b;
-let a = c;
+let [a,b,c] = [5,6,7];
+let [ra,rb,rc] = [a,b,c];
+
+b = ra; // 5
+c = rb; // 6
+a = rc; // 7
 
 console.log(a)
 console.log(b)
