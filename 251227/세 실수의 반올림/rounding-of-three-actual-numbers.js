239a72e0fcd2b07aa21b36269105b742d4fac774
lunainwoozu <78474488+lunainwoozu@users.noreply.github.com>
2025-12-27 14:40:55 +0900

    Title: 세 실수의 반올림, Time: 65ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251227/세 실수의 반올림/rounding-of-three-actual-numbers.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().split('\n');
+
+const a = Number(input[0]);
+const b = Number(input[1]);
+const c = Number(input[2]);
+
+console.log(a.toFixed(3))
+console.log(b.toFixed(3))
+console.log(c.toFixed(3))
