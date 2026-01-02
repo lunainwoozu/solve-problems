f0c7ff14625190e3548d492f816091f79ce5f0b5
lunainwoozu <78474488+lunainwoozu@users.noreply.github.com>
2026-01-02 15:04:53 +0900

    Title: 두 사람, Time: 63ms, Memory: 10MB, Status: Passed - Codetree

--- 260102/두 사람/two-person.js
+++ 260102/두 사람/two-person.js
@@ -4,7 +4,7 @@ const input = fs.readFileSync(0).toString().trim().split('\n');
 const [aA, sA] = input[0].split(' ');
 const [aB, sB] = input[1].split(' ');
 
-if ((aA >= 19 || aB >= 19) && (sA == 'M' || sB == 'M')){
+if ((aA >= 19 && sA == 'M') || (aB >= 19 && sB == 'M')){
     console.log(1);
 } else {
     console.log(0);
