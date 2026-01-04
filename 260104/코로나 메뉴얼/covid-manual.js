82622657ba45940b87c23302c831a2f157309d24
lunainwoozu <78474488+lunainwoozu@users.noreply.github.com>
2026-01-04 23:03:52 +0900

    Title: 코로나 메뉴얼, Time: 63ms, Memory: 10MB, Status: Passed - Codetree

--- 260104/코로나 메뉴얼/covid-manual.js
+++ 260104/코로나 메뉴얼/covid-manual.js
@@ -1,19 +1,14 @@
 const fs = require('fs');
 const input = fs.readFileSync(0).toString().trim().split('\n');
 
-const f = input[0].split(' ');
-const s = input[1].split(' ');
-const t = input[2].split(' ');
-
-const array = [f, s, t];
-
 let cnt = 0;
 
-for(let i = 0; i < 3; i++){
-    if(array[i][0] === 'Y' && array[i][1] >= 37){
+input.map(i => {
+    const [q, temp] = i.split(' ');
+    if(q === 'Y' && temp >= 37){
         cnt++;
     }
-}
+});
 
 if(cnt >= 2){
     console.log('E');
