4e1c6a5c947e56dbfd95a06fbf9bb49a79256ce5
lunainwoozu <78474488+lunainwoozu@users.noreply.github.com>
2026-01-11 19:38:26 +0900

    Title: 2 또는 3의 배수, Time: 62ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260111/2 또는 3의 배수/multiple-of-2-or-3.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const n = Number(fs.readFileSync(0));
+
+let result = '';
+
+for(let i = 1; i <= n; i++){
+    if (i % 2 === 0 || i % 3 === 0){
+        result += 1 + ' ';
+    } else {
+        result += 0 + ' ';
+    }
+}
+
+console.log(result);
