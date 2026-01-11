62c323a6d79b1e1e19c7688f7e1fef272d7e1b8d
lunainwoozu <78474488+lunainwoozu@users.noreply.github.com>
2026-01-11 20:57:46 +0900

    Title: 369 게임, Time: 67ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260111/369 게임/369-game.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const n = Number(fs.readFileSync(0));
+
+let result = '';
+
+for (let i = 1; i < n + 1; i++){
+    if (i % 3 === 0 || i.toString().includes('3') || i.toString().includes('6') || i.toString().includes('9')){
+        result += 0 + ' ';
+    } else {
+        result += i + ' ';
+    }
+}
+
+console.log(result);
