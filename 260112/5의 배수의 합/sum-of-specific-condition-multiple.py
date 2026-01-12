8a46b442f7e787b070e08b91fdd813ae30f1bc6d
lunainwoozu <78474488+lunainwoozu@users.noreply.github.com>
2026-01-12 15:44:58 +0900

    Title: 5의 배수의 합, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

--- 260112/5의 배수의 합/sum-of-specific-condition-multiple.py
+++ 260112/5의 배수의 합/sum-of-specific-condition-multiple.py
@@ -1,13 +1,10 @@
 a, b = map(int, input().split())
 sum = 0
 
-if a > b:
-    for i in range(b, a + 1):
-        if i % 5 == 0:
-            sum += i
-else:
-    for i in range(a, b + 1):
-        if i % 5 == 0:
-            sum += i
+Max, Min = max(a,b), min(a,b)
+
+for i in range(Min, Max + 1):
+    if i % 5 == 0:
+        sum += i
 
 print(sum)
