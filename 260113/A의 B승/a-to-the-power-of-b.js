09306ba8943a0d41cbe20615ff10c053bb58f6c5
lunainwoozu <78474488+lunainwoozu@users.noreply.github.com>
2026-01-13 15:57:06 +0900

    Title: A의 B승, Time: 66ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 260113/A의 B승/a-to-the-power-of-b.js
+++ 260113/A의 B승/a-to-the-power-of-b.js
@@ -3,7 +3,7 @@ const [a, b] = fs.readFileSync(0).toString().trim().split(' ').map(Number);
 
 let prod = a;
 
-for (let i = 1; i < b; i++) {
+for (let i = 0; i < b - 1; i++) {
     if (b === 0) break;
     prod = prod * a;
 }
