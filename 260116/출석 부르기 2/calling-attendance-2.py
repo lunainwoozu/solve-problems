04945cffeffda7891a52d5edc6873482a151d7d8
lunainwoozu <78474488+lunainwoozu@users.noreply.github.com>
2026-01-16 17:26:29 +0900

    Title: 출석 부르기 2, Time: 48ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 260116/출석 부르기 2/calling-attendance-2.py
@@ -0,0 +1,13 @@
+while True:
+    n = int(input())
+    if n > 4:
+        print('Vacancy')
+        break
+    elif n == 1:
+        print('John')
+    elif n == 2:
+        print('Tom')
+    elif n == 3:
+        print('Paul')
+    elif n == 4:
+        print('Sam')
