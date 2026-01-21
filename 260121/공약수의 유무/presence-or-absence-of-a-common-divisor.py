9ff64ef4e8c9bfc3bc960057d101dd2b99303a1a
lunainwoozu <78474488+lunainwoozu@users.noreply.github.com>
2026-01-21 12:42:54 +0900

    Title: 공약수의 유무, Time: 46ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 260121/공약수의 유무/presence-or-absence-of-a-common-divisor.py
+++ 260121/공약수의 유무/presence-or-absence-of-a-common-divisor.py
@@ -5,10 +5,11 @@ result = False
 for i in range(a, b + 1):
     if 1920%i==0 and 2280%i==0:
         result = True
-        print(1)
         break
 
-if result != True:
+if result:
+    print(1)
+else:
     print(0)
 
     
