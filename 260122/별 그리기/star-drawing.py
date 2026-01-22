0cc7dfcd79c31e83a2fc039b867519d7778333bf
lunainwoozu <78474488+lunainwoozu@users.noreply.github.com>
2026-01-22 19:05:26 +0900

    Title: 별 그리기, Time: 73ms, Memory: 18MB, Status: Passed - Codetree

--- 260122/별 그리기/star-drawing.py
+++ 260122/별 그리기/star-drawing.py
@@ -7,9 +7,9 @@ for i in range(1, n+1):
         print("*", end="")
     print()
 
-for i in range(n-1):
-    for _ in range(i+1):
+for i in range(n-1, 0, -1):
+    for _ in range(n-i):
         print(" ", end="")
-    for _ in range(n-i*2):
+    for _ in range(i*2-1):
         print("*", end="")
     print()
