0b3e0a8570d5f642d0c82fe4cd0f5cdf07dba426
lunainwoozu <78474488+lunainwoozu@users.noreply.github.com>
2026-03-09 17:07:16 +0900

    [level 0] Title: 분수의 덧셈, Time: 55.90 ms, Memory: 9.05 MB -BaekjoonHub

---
+++ Python3/프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.py
@@ -0,0 +1,11 @@
+def solution(numer1, denom1, numer2, denom2):
+    num1 = numer1 * denom2 + numer2 * denom1
+    num2 = denom1 * denom2
+    
+    gcd = 1
+    for i in range(1, min(num1, num2) + 1):
+        if num1 % i == 0 and num2 % i == 0:
+            gcd = i
+    
+    answer = [num1 / gcd, num2 / gcd]
+    return answer
