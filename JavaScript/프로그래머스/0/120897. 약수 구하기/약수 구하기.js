dee79db2a9a93542cdbef54e57464a6cc6102747
lunainwoozu <78474488+lunainwoozu@users.noreply.github.com>
2026-04-27 16:09:38 +0900

    [level 0] Title: 약수 구하기, Time: 0.46 ms, Memory: 33.4 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/120897. 약수 구하기/약수 구하기.js
+++ JavaScript/프로그래머스/0/120897. 약수 구하기/약수 구하기.js
@@ -1,7 +1,7 @@
 function solution(n) {
     let array = [];
     for(let i = 1; i <= n; i++){
-        if(n % i === 0) array.push(Math.floor(n/i));
+        if(n % i === 0) array.push(i);
     }
-    return array.sort((a, b) => a - b);
+    return array;
 }
