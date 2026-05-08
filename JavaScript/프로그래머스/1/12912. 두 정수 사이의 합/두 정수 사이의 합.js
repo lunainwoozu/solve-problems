44c3af6f8b155a6bf30ed3f076e877e76092bec6
lunainwoozu <78474488+lunainwoozu@users.noreply.github.com>
2026-05-08 11:20:36 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 872.82 ms, Memory: 184 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.js
@@ -0,0 +1,5 @@
+function solution(a, b) {
+    const max = Math.max(a,b);
+    const min = Math.min(a,b);
+    return Array.from({length: max - min + 1}, (_, i) => min + i).reduce((a,c) => a+c, 0)
+}
