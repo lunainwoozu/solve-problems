efcd81b68ab39fa6b11eb2b86ea80f71d829d001
lunainwoozu <78474488+lunainwoozu@users.noreply.github.com>
2026-06-01 17:48:46 +0900

    [level 2] Title: 올바른 괄호, Time: 4.92 ms, Memory: 38.8 MB -BaekjoonHub

--- JavaScript/프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
+++ JavaScript/프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,10 +1,9 @@
 function solution(s){
-    let lr = 0;
-    const tof = s.split("");
-    for(const w of tof) {
-        if (w === "(") lr++;
-        if (w === ")") lr--;
-        if (lr < 0) break;
-    }
-    return lr === 0 ? true : false;
+	let lr = 0;
+	const arr = s.split("");
+	for(const w of arr) {
+		w === "(" ? lr++ : lr--;
+		if (lr < 0) return false;
+	}
+	return lr === 0 ? true : false;
 }
